338bb36baa4b4c2fd07c48c972ef2c09668d9f25
Rishi Pande <rishi@fryol.com>
2024-01-15 22:30:08 -0800

    * cleaning up some comments/var names and adding a check in list_sub_dirs, just in case

--- scripts/list_all_sub_dirs.sh
+++ scripts/list_all_sub_dirs.sh
@@ -18,6 +18,11 @@ function list_sub_dirs() {
     dirs=();
     source_dir=$1;
 
+    if [ ! -d "$source_dir" ]; then
+        echo "FATAL(list_sub_dirs()): $source_dir is not a directory.";
+        exit 1;
+    fi
+
     for each_file in `\ls $source_dir`; do
         if [ -d "$source_dir/$each_file" ]; then 
             dirs+=( "$source_dir/$each_file" ); 
@@ -37,32 +42,35 @@ function main() {
         exit 1;
     fi
 
-    all_sub_dirs=()
+    all_sub_dirs_collection=()
     immediate_sub_dirs=()
-    dirs_not_visited=()
+    dirs_not_visited_queue=()
 
     top_dir_fqdn=`readlink -f $top_dir`;
-    dirs_not_visited+=( $top_dir_fqdn )
-    all_sub_dirs+=( $top_dir_fqdn )
+    dirs_not_visited_queue+=( $top_dir_fqdn )
+    all_sub_dirs_collection+=( $top_dir_fqdn )
 
-    # depth first kinda algo
-    # visit every dir in dirs_not_visited, and collect immediate
-    # sub_dirs under each of them into dirs_not_visited, until
+    # breadth first kinda algo
+    # visit every dir in dirs_not_visited_queue, and collect immediate
+    # sub_dirs under each of them into dirs_not_visited_queue, until
     # all sub_dirs are visited
 
-    while [ ${#dirs_not_visited[@]} -ne 0 ]; do
-        immediate_sub_dirs=$(list_sub_dirs "${dirs_not_visited[0]}");
+    while [ ${#dirs_not_visited_queue[@]} -ne 0 ]; do
+        immediate_sub_dirs=$(list_sub_dirs "${dirs_not_visited_queue[0]}");
+        
+        # pushing immediate_sub_dirs found into dirs_not_visited_queue array 
+        dirs_not_visited_queue+=( ${immediate_sub_dirs[@]} );
+        
+        # adding all found immediate_sub_dirs into all_sub_dirs_collection because
+        # dirs_not_visited_queue is well, a queue, that will be empty at some point
+        all_sub_dirs_collection+=( ${immediate_sub_dirs[@]} );
 
-        dirs_not_visited+=( ${immediate_sub_dirs[@]} );
-
-        all_sub_dirs+=( ${immediate_sub_dirs[@]} );
-
-        # popping the top of dirs_not_visited (dirs_not_visited[0]), 
+        # popping the top of dirs_not_visited_queue (dirs_not_visited_queue[0]), 
         # since it was just visited above when list_sub_dirs was called
-        dirs_not_visited=( ${dirs_not_visited[@]:1} );
+        dirs_not_visited_queue=( ${dirs_not_visited_queue[@]:1} );
     done
 
-    for each_dir in "${all_sub_dirs[@]}"; do
+    for each_dir in "${all_sub_dirs_collection[@]}"; do
         echo $each_dir;
     done
 }
